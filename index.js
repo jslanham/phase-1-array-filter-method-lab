d25c6af0e4bf2e4020bfc40efeecb5cc89d690a3
Joseph Lanham <jslanham2020@gmail.com>
2024-02-15 19:45:13 -0500

    filtered results

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const findMatching = (drivers, string) => {
+    return drivers.filter(name => name.toLowerCase() == string.toLowerCase())
+}
+const fuzzyMatch = (drivers, string) => {
+    return drivers.filter(name =>{
+        return name.slice(0, string.length) === string
+    })
+}
+const matchName = (drivers, string) => {
+    return drivers.filter (item => {
+        return item.name === string
+    })
+}
